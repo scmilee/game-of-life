41cdc561f3b40d558d28ab9d377985c3ea9245f5
scmilee <drudged@oregonstate.edu>
2018-06-11 14:29:04 -0700

    add cell neighbors and a has neighbors method

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,18 +1,24 @@
 # TODO
 
 class Cell
-    attr_accessor :alive
+    attr_accessor :alive, :neighbors
 
   def initialize()
     @alive= true
+    @neighbors = []
   end
 
   def is_alive?
     alive
   end
 
+  def has_neighbors?
+    neighbors.length > 0
+  end
+
   def kill
     @alive = false
   end
+
   
 end
